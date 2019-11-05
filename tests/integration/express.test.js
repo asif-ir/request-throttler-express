a55210d1916cb1b0ae2e6959fb3c67bee5c81b2a
md.rahman <md.rahman@thoughtworks.com>
2019-11-05 14:11:16 +0530

    Fix integration test

--- tests/integration/express.test.js
+++ tests/integration/express.test.js
@@ -1,23 +1,9 @@
 const axios = require('axios');
-const redis = require('redis');
 const PORT = 3000;
 const API_URL = `http://localhost:${PORT}`;
 const maxHits = 3;
 
-const client = redis.createClient();
-
 describe('Should block after rate limit exceeds ::integration', () => {
-    beforeEach(async (done) => {
-        const localIp = '::ffff:127.0.0.1';
-        client.del([localIp], (err) => {
-            if (!err) {
-                client.keys('*', (err, res) => {
-                    console.log('keys', res);
-                });
-                done();
-            }
-        });
-    });
 
     it('should return 200 for first 3 requests and 429 for rest', async function () {
         for (let i = maxHits; i > 0; --i) {
